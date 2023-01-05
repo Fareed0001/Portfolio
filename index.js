4f0a4887d5cd2d8b2024e87574e4c4aefb953765
Farid Ibrahim <fareedibrahim0003@gmail.com>
2023-01-05 11:58:10 +0100

    First upload

---
+++ index.js
@@ -0,0 +1,80 @@
+// CAROUSEL
+document.addEventListener('DOMContentLoaded', () => {
+
+  //===== MICRO-SLIDER begin
+	const __ms = document.querySelector('.micro-slider');
+  const __msSlider = new MicroSlider(__ms, { indicators: true, indicatorText: '' });
+  const hammer = new Hammer(__ms);
+	const __msTimer = 2000;
+  let __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+
+  //detect mouseenter event
+	__ms.onmouseenter = function(e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + ' mouse detected');
+  }
+
+  //detect mouseleave event
+	__ms.onmouseleave = function(e) {
+    clearInterval(__msAutoplay);
+    __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    console.log(e.type + ' mouse detected');
+  }
+
+  //detect mouseclick event
+	__ms.onclick = function(e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + ' mouse detected');
+  }
+
+  //detect gesture tap event with hammer js library
+  hammer.on('tap', function(e) {
+    clearInterval(__msAutoplay);
+    console.log(e.type + ' gesture detected');
+  });
+
+  //detect gesture swipe event with hammer js library
+  hammer.on('swipe', function(e) {
+    clearInterval(__msAutoplay);
+    __msAutoplay = setInterval(() => __msSlider.next(), __msTimer);
+    console.log(e.type + ' gesture detected');
+  });
+
+  let slideLink = document.querySelectorAll('.slider-item');
+  if (slideLink && slideLink !== null && slideLink.length > 0){
+    slideLink.forEach( el => el.addEventListener('click', e => {
+      e.preventDefault();
+      let href = el.dataset.href;
+      let target = el.dataset.target;
+      if (href !== '#') window.open(href, target);
+    }));
+  }
+  //===== MICRO-SLIDER end
+});
+// CAROUSEL END
+
+// SIXTH DIV
+$(".sixth-list1, .sixth-list2").slideUp();
+$(".sixth-close-button1, .sixth-close-button2").css("display", "none");
+
+$(".sixth-button1").on("click", function() {
+	$(".sixth-list1").slideDown();
+	$(".sixth-button1").css("display", "none");
+	$(".sixth-close-button1").css("display", "block");
+});
+$(".sixth-button2").on("click", function() {
+	$(".sixth-list2").slideDown();
+	$(".sixth-button2").css("display", "none");
+	$(".sixth-close-button2").css("display", "block");
+});
+
+$(".sixth-close-button1").on("click", function() {
+	$(".sixth-list1").slideUp();
+	$(".sixth-button1").css("display", "block");
+	$(".sixth-close-button1").css("display", "none");
+});
+$(".sixth-close-button2").on("click", function() {
+	$(".sixth-list2").slideUp();
+	$(".sixth-button2").css("display", "block");
+	$(".sixth-close-button2").css("display", "none");
+});
